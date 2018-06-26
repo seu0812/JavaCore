e4e49f212ce567fda0a5fbea4a41059436125dc8
suhova <suhova@RDP>
2018-06-26 12:25:39 +0600

    TypeData

--- src/lesson/main.java
+++ src/lesson/main.java
@@ -3,7 +3,22 @@ package lesson;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s = "38";
+		String a = "78";
+		int b = Integer.parseInt(a);
+		int c = Integer.parseInt(s);
+		System.out.println(b+c);
+		String x = String.valueOf(b);
+		System.out.println(x+x+x);
+		double d = Double.parseDouble("78");
+		System.out.println(d);
+		float n = (float) 34.5587;
+		System.out.println(n);
+		
+		
+		
+		
+		
 
 	}
 
