bc2994ab93e57d04d848871d656477e1d36dfec5
suhova <suhova@RDP>
2018-08-15 16:15:16 +0600

    подсчет папок и файлов

--- src/CountFiles/CountFiles.java
+++ src/CountFiles/CountFiles.java
@@ -1,10 +1,10 @@
-package ProgrammFile;
+package CountFiles;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Program {
+public class CountFiles {
 	
 	static List<File> files = new ArrayList<>();
 
